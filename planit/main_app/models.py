2bb15240ee97c6ab7b6fdae93a080cabcd269683
jbroom86 <jbroom86@gmail.com>
2023-03-31 16:06:21 -0700

    set up Event model

--- planit/main_app/models.py
+++ planit/main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    who = models.CharField(max_length=150)
+    what = models.CharField(max_length=150)
+    where = models.CharField(max_length=100)
+    when = models.CharField(max_length=150)
+    why = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.what
+    
+# class Poll(models.Model):
+
+
+# class Group(models.Model):
