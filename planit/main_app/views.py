153a696dc5a4558a778c8f0b07a9e289f3f59f66
jonah-angelo <jonah.angelo@gmail.com>
2023-04-02 11:48:57 -0700

    Added about view and url

--- planit/main_app/views.py
+++ planit/main_app/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 
 # Home view
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+  
