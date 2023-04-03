0e52ec371b1eefbab69938856940153b580e4c56
jonah-angelo <jonah.angelo@gmail.com>
2023-04-03 09:05:07 -0700

    Added url/views/folder for events index

--- planit/main_app/views.py
+++ planit/main_app/views.py
@@ -9,4 +9,9 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
-  
+
+def events_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'events/index.html', {
+    'events': events
+  })
